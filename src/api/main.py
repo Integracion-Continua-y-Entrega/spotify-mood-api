b232c7ec9bad49db7e5b350af0eeedcdffdb757d
Carlos M <carlosmi.be13@gmail.com>
2026-06-02 20:22:32 -0600

    Add production frontend CORS origin

--- src/api/main.py
+++ src/api/main.py
@@ -42,7 +42,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173", "http://localhost:5173", "http://0.0.0.0:5173"],
+    allow_origins=[
+        "http://127.0.0.1:5173",
+        "http://localhost:5173",
+        "http://0.0.0.0:5173",
+        "https://spotify-mood-api-1.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
